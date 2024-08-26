0ed8f6db605a40bc5270f42b9ea9310f4d72e786
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-08-26 00:25:32 -0300

    cenário de teste EmpilharElemento adicionado

--- eclipse-workspace/TDD-Java/src/Pilha.java
+++ eclipse-workspace/TDD-Java/src/Pilha.java
@@ -2,12 +2,25 @@
 
 public class Pilha {
 
+	private String pilhaValue; 
+	private int quantidade;
+	
+	
 	public Boolean isVazia() {
-		return true;
+		return (pilhaValue == null); // equivale a: (pilhaValue == null) ? true : false; 
 	}
 
-	public Integer tamanho() {
-		return 0;
+	public int tamanho() {
+		return this.quantidade; //retorna a quantidade na pilha
+	}
+
+	public void empilhar(String pilhaValue) { //FIXME: Talvez seja necessário converter para Object 
+		this.pilhaValue = pilhaValue; //guardo o valor da pilha recebido, pois será utilizado futuramente
+		this.quantidade ++; //incrementa a quantidade
+	}
+
+	public Object topoDaPilha() {
+		return this.pilhaValue;
 	}
 
 }
--- eclipse-workspace/TDD-Java/src/TestPilha.java
+++ eclipse-workspace/TDD-Java/src/TestPilha.java
@@ -7,14 +7,23 @@ class TestPilha {
 	// verifica se a pilha está vazia
 	@Test
 	void pilhaVazia() {
-		//inicializa a classe pilha com capacidade 10, neste caso de arrays
-		Pilha pilha = new Pilha();
 		
-		//verifica se o método retorna 'true'. O método verifica se a pilha está vazia.
-		assertTrue(pilha.isVazia());
+		Pilha pilha = new Pilha(); //inicializa a classe pilha com capacidade 10, neste caso de arrays
 		
-		//verifica se o length da pilha é igual a 0
-		assertEquals(0,pilha.tamanho());
+		assertTrue(pilha.isVazia()); 	//verifica se o método retorna 'true'. O método verifica se a pilha está vazia.
+		assertEquals(0,pilha.tamanho()); 	//verifica se o length da pilha é igual a 0
 	}
 
+	
+	@Test
+	void empilharElemento() {
+		
+		Pilha pilha = new Pilha();
+		String pilhaValue = "Primeiro"; 
+		
+		pilha.empilhar(pilhaValue); 	// empilhará elementos de string
+		assertFalse(pilha.isVazia()); // verificar se o método retorna 'false'. O método verifica se a pilha está vazia
+		assertEquals(1,pilha.tamanho()); 	//verifica se o length da pilha é igual a 1
+		assertEquals(pilhaValue, pilha.topoDaPilha());//verifica se o valor do primeiro item da pilha
+	}
 }
