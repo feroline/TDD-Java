5c70e17abfdd45fb40ea5b8d78a3c4b12d1dace7
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-08-25 23:54:32 -0300

    adicionando testes antes de compilar

--- eclipse-workspace/TDD-Java/src/TestPilha.java
+++ eclipse-workspace/TDD-Java/src/TestPilha.java
@@ -4,9 +4,17 @@ import org.junit.jupiter.api.Test;
 
 class TestPilha {
 
+	// verifica se a pilha está vazia
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void pilhaVazia() {
+		//inicializa a classe pilha com capacidade 10, neste caso de arrays
+		Pilha pilha = new Pilha(10);
+		
+		//verifica se o método retorna 'true'. O método verifica se a pilha está vazia.
+		assertTrue(pilha.isVazia());
+		
+		//verifica se o length da pilha é igual a 0
+		assertEquals(0,pilha.tamanho());
 	}
 
 }
