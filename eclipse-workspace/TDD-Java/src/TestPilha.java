1faae5bda2ac70b4573b3ab2a6d40fdb6a58d11b
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-08-28 17:21:21 -0300

    refatorando testes

--- eclipse-workspace/TDD-Java/src/TestPilha.java
+++ eclipse-workspace/TDD-Java/src/TestPilha.java
@@ -7,18 +7,36 @@ import org.junit.jupiter.api.Test;
 class TestPilha {
 	
 	private Pilha pilha; 
+	private int tamanhoMax = 10;
+	private String msgExceptionPilhaCheia = "A pilha atingiu o limite e estourou :("; 
+	private String msgExceptionPilhaVazia = "A pilha está vazia e não é possível desempilhar";
+	
+	private String getMsgException(RuntimeException exception) {
+		return exception.getMessage();
+	}
+	
+	private void empilharByQuant(int quantidade) {
+		for(int i = 0; i < quantidade; i++) {
+			pilha.empilhar("conteúdo "+ i);
+		}
+		
+	}
+
+	private void verificaTamanhoPilha(int tamanhoEsperado) {
+		assertEquals(tamanhoEsperado,pilha.tamanho());
+	}
+	
 	
 	@BeforeEach
 	public void inicializarPilha() {
-		pilha = new Pilha(10); //inicializa a pilha com capacidade de 10
+		pilha = new Pilha(tamanhoMax); 
 	}
 
-	// verifica se a pilha está vazia
 	@Test
-	void pilhaVazia() {
+	void isPilhaVazia() {
 		
-		assertTrue(pilha.isVazia()); 	//verifica se o método retorna 'true'. O método verifica se a pilha está vazia.
-		assertEquals(0,pilha.tamanho()); 	//verifica se o length da pilha é igual a 0
+		assertTrue(pilha.isVazia()); 	
+		verificaTamanhoPilha(0);
 	}
 
 	
@@ -26,15 +44,15 @@ class TestPilha {
 	void empilharElemento() {
 		
 		Object pilhaValue = "Primeiro"; 
+		pilha.empilhar(pilhaValue); 	
 		
-		pilha.empilhar(pilhaValue); 	// empilhará elementos de string
-		assertFalse(pilha.isVazia()); // verificar se o método retorna 'false'. O método verifica se a pilha está vazia
-		assertEquals(1,pilha.tamanho()); 	//verifica se o length da pilha é igual a 1
-		assertEquals(pilhaValue, pilha.topoDaPilha());//verifica se o valor do primeiro item da pilha
+		assertFalse(pilha.isVazia()); 
+		verificaTamanhoPilha(1);
+		assertEquals(pilhaValue, pilha.topoDaPilha());
 	}
 	
 	@Test
-	void empilhaDoisElementos() {
+	void empilharAndDesempilhar() {
 		
 		Object value1 = "Primeiro";
 		Object value2 = "Segundo"; 
@@ -42,46 +60,39 @@ class TestPilha {
 		//empilha 2 e verifica
 		pilha.empilhar(value1);
 		pilha.empilhar(value2);	
-		assertEquals(2, pilha.tamanho());
+		
+		verificaTamanhoPilha(2);
 		assertEquals(value2,pilha.topoDaPilha());
 		
 		//desempilha 1 e verifica 
-		Object desempilhado = pilha.desempilhar();
-		assertEquals(1,pilha.tamanho());
+		pilha.desempilhar();
+		
+		verificaTamanhoPilha(1);
 		assertEquals(value1, pilha.topoDaPilha());
 		
 	}
 	
 	@Test
 	void desempilharPilhaVazia() {
-		String mensagemEsperada = "A pilha está vazia e não é possível desempilhar";
 		
-		// verifica se a assertion retornada corresponde a assertion desejada
 		PilhaVaziaException exception = assertThrows(PilhaVaziaException.class, () -> pilha.desempilhar());
-		
-	    String mensagemRecebida = exception.getMessage();
-	    assertTrue(mensagemRecebida.contains(mensagemEsperada)); //verifica se a mensagem de erro da assertion retornada é igual a mensagem desejada
-
+	    assertTrue(getMsgException(exception).contains(msgExceptionPilhaVazia));
+	    
 	}
 	
 	@Test
 	void encherPilha() {
-		String mensagemEsperada = "A pilha atingiu o limite e estourou :("; 
-		
-		for(int i = 0; i < 10; i++) {
-			pilha.empilhar("conteúdo "+ i);
-		}
+
+		empilharByQuant(tamanhoMax);
 		
 		try{
 			pilha.empilhar("BOOOM!! Estourou"); 
 			fail(); //se não deu a exceção irá para o catch
 		}catch(PilhaCheiaException exception) {
-			String mensagemRecebida = exception.getMessage();
-			assertTrue(mensagemRecebida.contains(mensagemEsperada));
+			assertTrue(getMsgException(exception).contains(msgExceptionPilhaCheia));
 		}
-		
 	
 	}
-
+	
 	
 }
